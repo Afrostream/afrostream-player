28eae816e9224499f63914e2e09f3c34bb702d27
benjipott <pott.benjamin@gmail.com>
2015-11-04 09:55:22 +0100

    rebuild player

--- dist/afrostream-player.js
+++ dist/afrostream-player.js
@@ -15,7 +15,7 @@
 
   // Pass this if window is not defined yet
 }(typeof window !== 'undefined' ? window : this, function (window, noGlobal) { /*jshint unused:false*/
-  /*! afrostream-player - v1.0.5 - 2015-11-03
+  /*! afrostream-player - v1.0.5 - 2015-11-04
 * Copyright (c) 2015 benjipott; Licensed Apache-2.0 */
 // HTML5 Shiv. Must be in <head> to support older browsers.
 document.createElement('video');
