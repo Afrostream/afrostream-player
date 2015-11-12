821a3300dfe5af8f8d343daf9b88030bf6bc18b2
benjipott <pott.benjamin@gmail.com>
2015-11-12 13:10:21 +0100

    bump version with audio swich button

--- demo/demo.js
+++ demo/demo.js
@@ -62,10 +62,10 @@ var loadPlayer = function (url) {
         "src": "https://origin.cdn.afrostream.net/vod/ARAISININTHESUN_178_25_ProRes422_FRA_ENG_HD_STEREO/0341bc2bdadd2e79.ism/master.m3u8",
         "type": "application/vnd.apple.mpegurl"
       },
-      //{
-      //  "src": "https://origin.cdn.afrostream.net/vod/ARAISININTHESUN_178_25_ProRes422_FRA_ENG_HD_STEREO/0341bc2bdadd2e79.ism/0341bc2bdadd2e79.mpd",
-      //  "type": "application/dash+xml"
-      //}
+      {
+        "src": "https://origin.cdn.afrostream.net/vod/ARAISININTHESUN_178_25_ProRes422_FRA_ENG_HD_STEREO/0341bc2bdadd2e79.ism/0341bc2bdadd2e79.mpd",
+        "type": "application/dash+xml"
+      }
     ];
 
   var techOrder = qs.tech ? qs.tech.split(',') : ['dash', 'html5'];
