8c71747d9173f9cba2ed33c151f642ff7af8fa7d
benjipott <pott.benjamin@gmail.com>
2015-12-04 05:36:27 +0100

    remove postinstall script

--- dist/afrostream-player.castlab.js
+++ dist/afrostream-player.castlab.js
@@ -1,4 +1,4 @@
-/*! afrostream-player - v1.0.20 - 2015-12-03
+/*! afrostream-player - v1.1.0 - 2015-12-04
 * Copyright (c) 2015 benjipott; Licensed Apache-2.0 */
 // HTML5 Shiv. Must be in <head> to support older browsers.
 document.createElement('video');
