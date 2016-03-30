46a825497a7e6204f3af1984ebfb5d926b2214a2
benjipott <pott.benjamin@gmail.com>
2016-03-30 09:31:14 +0200

    get thumbnail map from currentSrc

--- es5/js/component/control-bar/progress-control/mouse-thumbnail-display.js
+++ es5/js/component/control-bar/progress-control/mouse-thumbnail-display.js
@@ -9,7 +9,7 @@ Object.defineProperty(exports, '__esModule', {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -44,16 +44,64 @@ var MouseThumbnailDisplay = (function (_MouseTimeDisplay) {
     _get(Object.getPrototypeOf(MouseThumbnailDisplay.prototype), 'constructor', this).call(this, player, options);
   }
 
-  //Push videojs SeekBar child with this one
-
-  /**
-   * Create the component's DOM element
-   *
-   * @return {Element}
-   * @method createEl
-   */
-
   _createClass(MouseThumbnailDisplay, [{
+    key: 'createLoader',
+    value: function createLoader(src) {
+      this.destroyLoader();
+
+      this.img = new Image();
+      this.img.onload = this.handleComplete.bind(this);
+      this.img.onerror = this.handleError.bind(this);
+      this.img.src = src;
+    }
+  }, {
+    key: 'handleComplete',
+    value: function handleComplete() {
+      var url = this.destroyLoader();
+      _videoJs2['default'].log('thumbnails : next complete ' + url);
+    }
+  }, {
+    key: 'handleError',
+    value: function handleError() {
+      var url = this.destroyLoader();
+      _videoJs2['default'].log('thumbnails : next error ' + url);
+    }
+  }, {
+    key: 'destroyLoader',
+    value: function destroyLoader() {
+      var imgSrouce = '';
+      if (this.img) {
+        imgSrouce = this.img.src;
+        this.img.onload = null;
+        this.img.onerror = null;
+        this.img = null;
+      }
+      return imgSrouce;
+    }
+  }, {
+    key: 'extractAssetUri',
+    value: function extractAssetUri() {
+      var max = arguments.length <= 0 || arguments[0] === undefined ? 1 : arguments[0];
+
+      var currentSrc = this.player_.currentSrc();
+      var urlInfo = _videoJs2['default'].parseUrl(currentSrc);
+      var pathname = urlInfo.pathname.replace(/\/([a-z0-9\/\._-]{16}\.[is]sml?)+\/([a-z0-9\/\._-]{16}\.(mpd|m3u8)?)$/gi, '');
+      var fullPah = urlInfo.protocol + '//' + urlInfo.host + pathname + '/frames/map-{index}.jpg';
+      var current = fullPah.replace('{index}', this.itemIndex);
+      var next = fullPah.replace('{index}', this.itemIndex + 1);
+      if (this.itemIndex < max) {
+        this.createLoader(next);
+      }
+      return current;
+    }
+
+    /**
+     * Create the component's DOM element
+     *
+     * @return {Element}
+     * @method createEl
+     */
+  }, {
     key: 'createEl',
     value: function createEl() {
       var el = _videoJs2['default'].createEl('div', {
@@ -87,17 +135,24 @@ var MouseThumbnailDisplay = (function (_MouseTimeDisplay) {
       var secondsPerSheet = timeInterval * spritesPerSheet;
 
       var index = Math.max(1, Math.ceil(newTime / secondsPerSheet));
+      var duration = this.player_.duration();
+      var maxItem = 1;
+      if (duration) {
+        maxItem = Math.ceil(this.player_.duration() / secondsPerSheet);
+      }
+      console.log('thumbnails :', maxItem);
       var stripedTime = newTime - (index - 1) * secondsPerSheet;
-      var sheetIndex = Math.ceil(stripedTime / 2);
+      var sheetIndex = Math.floor(stripedTime / timeInterval);
       var x = Math.floor(sheetIndex % 5 * sheetWidth);
       var y = Math.floor(sheetIndex / spritesPerCol) * sheetHeight;
-
       if (this.itemIndex !== index) {
         this.itemIndex = index;
-        var url = 'http://origin.afrostream.tv/vod/24hourlovebis/frames/map-' + this.itemIndex + '.jpg';
+        var url = this.extractAssetUri(maxItem);
+        console.log('thumbnails :', url);
         var backgroundImage = 'url("' + url + '")';
         this.fallbackImg_.style.backgroundImage = backgroundImage;
       }
+
       this.fallbackImg_.style.backgroundPositionX = -x + 'px';
       this.fallbackImg_.style.backgroundPositionY = -y + 'px';
     }
@@ -106,6 +161,8 @@ var MouseThumbnailDisplay = (function (_MouseTimeDisplay) {
   return MouseThumbnailDisplay;
 })(MouseTimeDisplay);
 
+MouseThumbnailDisplay.prototype.itemIndex = 1;
+//Push videojs SeekBar child with this one
 SeekBar.prototype.options_.children.push('mouseThumbnailDisplay');
 
 Component.registerComponent('MouseThumbnailDisplay', MouseThumbnailDisplay);
--- src/js/component/control-bar/progress-control/mouse-thumbnail-display.js
+++ src/js/component/control-bar/progress-control/mouse-thumbnail-display.js
@@ -22,6 +22,49 @@ class MouseThumbnailDisplay extends MouseTimeDisplay {
     super(player, options);
   }
 
+  createLoader(src) {
+    this.destroyLoader();
+
+    this.img = new Image();
+    this.img.onload = ::this.handleComplete;
+    this.img.onerror = ::this.handleError;
+    this.img.src = src;
+  }
+
+  handleComplete() {
+    const url = this.destroyLoader();
+    videojs.log('thumbnails : next complete ' + url)
+  }
+
+  handleError() {
+    const url = this.destroyLoader();
+    videojs.log('thumbnails : next error ' + url);
+  }
+
+  destroyLoader() {
+    let imgSrouce = '';
+    if (this.img) {
+      imgSrouce = this.img.src;
+      this.img.onload = null;
+      this.img.onerror = null;
+      this.img = null;
+    }
+    return imgSrouce;
+  }
+
+  extractAssetUri(max = 1) {
+    let currentSrc = this.player_.currentSrc();
+    let urlInfo = videojs.parseUrl(currentSrc);
+    let pathname = urlInfo.pathname.replace(/\/([a-z0-9\/\._-]{16}\.[is]sml?)+\/([a-z0-9\/\._-]{16}\.(mpd|m3u8)?)$/gi, '');
+    let fullPah = `${urlInfo.protocol}//${urlInfo.host}${pathname}/frames/map-{index}.jpg`;
+    let current = fullPah.replace('{index}', this.itemIndex);
+    let next = fullPah.replace('{index}', this.itemIndex + 1);
+    if (this.itemIndex < max) {
+      this.createLoader(next);
+    }
+    return current;
+  }
+
   /**
    * Create the component's DOM element
    *
@@ -59,22 +102,30 @@ class MouseThumbnailDisplay extends MouseTimeDisplay {
     const secondsPerSheet = timeInterval * spritesPerSheet;
 
     let index = Math.max(1, Math.ceil(newTime / secondsPerSheet));
+    let duration = this.player_.duration();
+    let maxItem = 1;
+    if (duration) {
+      maxItem = Math.ceil(this.player_.duration() / secondsPerSheet);
+    }
+    console.log('thumbnails :', maxItem)
     let stripedTime = newTime - ((index - 1) * secondsPerSheet);
-    let sheetIndex = Math.ceil(stripedTime / 2);
+    let sheetIndex = Math.floor(stripedTime / timeInterval);
     let x = Math.floor((sheetIndex % 5) * sheetWidth);
     let y = Math.floor(sheetIndex / spritesPerCol) * sheetHeight;
-
     if (this.itemIndex !== index) {
       this.itemIndex = index;
-      let url = `http://origin.afrostream.tv/vod/24hourlovebis/frames/map-${this.itemIndex}.jpg`;
+      let url = this.extractAssetUri(maxItem);
+      console.log('thumbnails :', url)
       let backgroundImage = `url("${url}")`;
       this.fallbackImg_.style.backgroundImage = backgroundImage;
     }
+
     this.fallbackImg_.style.backgroundPositionX = -x + 'px';
     this.fallbackImg_.style.backgroundPositionY = -y + 'px';
   }
 }
 
+MouseThumbnailDisplay.prototype.itemIndex = 1;
 //Push videojs SeekBar child with this one
 SeekBar.prototype.options_.children.push('mouseThumbnailDisplay');
 
